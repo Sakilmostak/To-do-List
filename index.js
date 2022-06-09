ae5b22948f02025a269d779f16d77e7c96144956
Sakil Mostak <skmahim71@gmail.com>
2022-06-09 15:54:40 +0530

    controller and route are added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Express = require('express');
 const app = Express();
 const port = 3000;
 
+//express router
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
